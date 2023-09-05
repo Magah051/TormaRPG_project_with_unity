c8df1f03f103db92a593455e5b8d8d911eaa582d
José Magalhães <joses.magalhaes@hotmail.com>
2023-09-05 13:29:46 -0300

    Corrigindo Bug de colisão

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     //Esse script controlará todos os tipos de Players
@@ -10,11 +11,14 @@ public class PlayerController : MonoBehaviour
     float input_y = 0;
     public float speed = 2.5f;
     bool isWalking = false;
+    Rigidbody2D rb2D;
+    Vector2 movement = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         isWalking = false;
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,11 +27,10 @@ public class PlayerController : MonoBehaviour
         input_x = Input.GetAxisRaw("Horizontal");
         input_y = Input.GetAxisRaw("Vertical");
         isWalking = (input_x != 0 || input_y != 0);
+        movement = new Vector2(input_x, input_y);
 
         if (isWalking)
         {
-            var move = new Vector3(input_x, input_y, 0).normalized;
-            transform.position += move * speed * Time.deltaTime;
             playerAnimator.SetFloat("input_x", input_x);
             playerAnimator.SetFloat("input_y", input_y);
         }
@@ -37,5 +40,10 @@ public class PlayerController : MonoBehaviour
         //O "hero" não possui ataque por enquanto (05/09/2023)
         if (Input.GetButtonDown("Fire1"))
             playerAnimator.SetTrigger("attack");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + movement * speed * Time.fixedDeltaTime);
     }
 }
